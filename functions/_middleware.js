baf8f79b2b6d7b4e6853255dca06cefc5ae04e7c
emptyphenomena <rgarg@alumni.berklee.edu>
2025-02-12 11:56:46 -0600

    “update”

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,24 +1,12 @@
-export async function onRequest({ request, env, next }) {
+export async function onRequest({ request, next }) {
     const url = new URL(request.url);
     
-    // Mappings both ways
-    const routes = {
-      '/reality': '/content/reality.html'
-    };
-    
-    // If someone lands on a content URL, redirect them to clean URL
-    if (url.pathname.startsWith('/content/')) {
-      const cleanPath = url.pathname
-        .replace('/content/', '/')
-        .replace('.html', '');
-      return Response.redirect(`${url.origin}${cleanPath}`, 301);
-    }
-  
-    // If this is a clean URL, handle it with internal rewrite
-    if (routes[url.pathname]) {
-      const newUrl = new URL(url.href);
-      newUrl.pathname = routes[url.pathname];
-      return fetch(new Request(newUrl, request));
+    // Simple one-way internal rewrite
+    if (url.pathname === '/reality') {
+      return next({
+        ...request,
+        url: `${url.origin}/content/reality.html`
+      });
     }
   
     return next(request);
