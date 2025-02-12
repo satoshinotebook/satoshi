3d65a7ddeda8080471b8ba70bfbf050d09d25493
emptyphenomena <rgarg@alumni.berklee.edu>
2025-02-12 11:34:29 -0600

    “update”

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,16 +1,19 @@
 export async function onRequest({ request, next }) {
     const url = new URL(request.url);
     
-    // If we're at /reality, internally rewrite to /content/reality.html
-    if (url.pathname === '/reality') {
-      url.pathname = '/content/reality.html';
-      request = new Request(url, request);
+    // Define a mapping of clean URLs to their content paths
+    const urlMapping = {
+      '/reality': '/content/reality.html'
+    };
+  
+    // Check if this is a clean URL we want to handle
+    if (urlMapping[url.pathname]) {
+      // Only rewrite the internal request, don't redirect
+      const newUrl = new URL(request.url);
+      newUrl.pathname = urlMapping[url.pathname];
+      return next(new Request(newUrl, request));
     }
-    
-    // If we're at /content/reality, redirect to /reality
-    if (url.pathname === '/content/reality') {
-      return Response.redirect(new URL('/reality', request.url), 301);
-    }
-    
+  
+    // For all other requests, just pass them through
     return next(request);
   }
